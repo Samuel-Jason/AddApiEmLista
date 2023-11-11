6c60a4fbe76b4ca566dc17e07dc001fcc0db849c
Samuel <samuelljason@gmail.com>
2023-11-11 02:13:03 -0300

    first commit

---
+++ teste1/MeuObjeto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teste1
+{
+    public class MeuObjeto : Slip
+    {
+        public Slip Slip { get; set; }
+    }
+
+    public class Slip
+    {
+        public int Id { get; set; }
+        public string Advice { get; set; }
+    }
+}
---
+++ teste1/Program.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace teste1
+{
+    internal class Program : MeuObjeto
+    {
+        static async Task Main(string[] args)
+        {
+            string apiUrl = "https://api.adviceslip.com/advice";
+
+            // Instanciar o HttpClient
+            using (HttpClient client = new HttpClient())
+            {
+                List<MeuObjeto> minhaLista = new List<MeuObjeto>();
+
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string conteudo = await response.Content.ReadAsStringAsync();
+                        
+                        MeuObjeto objetosDaConsulta = JsonConvert.DeserializeObject<MeuObjeto>(conteudo);
+                        minhaLista.Add(objetosDaConsulta);
+
+
+                        foreach(MeuObjeto item in minhaLista)
+                        {
+                            Console.WriteLine($"Id: {item.Id}, Advice: {item.Advice}");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+        }
+    }
+}
